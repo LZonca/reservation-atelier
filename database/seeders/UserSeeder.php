958635630ba181d60c333aa53c27628f3f92e418
LZonca <leo.zonca@business-school.academy>
2025-11-27 14:59:18 +0100

    ajout default user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,5 +10,11 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456789')
+        ]);
     }
 }
